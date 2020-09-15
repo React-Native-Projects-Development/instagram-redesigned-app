ed48ee9582d0c0fcf55faabe6b0daaee1a77d296
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-15 13:50:50 -0600

    Loading needed fonts

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import Header from "./components/Header";
+import { Ionicons } from "@expo/vector-icons";
+
+const loadFonts = async () =>
+  await Font.loadAsync({
+    Billabong: require("./assets/fonts/Billabong.ttf"),
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    ...Ionicons.font,
+  });
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <AppLoading startAsync={loadFonts} onFinish={() => setLoading(false)} />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -14,8 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });

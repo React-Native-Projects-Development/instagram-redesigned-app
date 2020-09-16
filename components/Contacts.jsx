8676ee83a38b8cccaefe6021c4d1b6055f4a496a
Brayan Gacia <bgarcia.95@outlook.com>
2020-09-15 18:15:16 -0600

    Adding Contacts component

---
+++ components/Contacts.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { usersData } from "../data/dummy-data";
+import AvatarStory from "./AvatarStory";
+
+const Contacts = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={usersData}
+        renderItem={({ item, index }) => (
+          <AvatarStory
+            index={index}
+            user={item.user}
+            profilePicture={item.profilePicture}
+          />
+        )}
+        horizontal
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    height: 125,
+  },
+});
+
+export default Contacts;

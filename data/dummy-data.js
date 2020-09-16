8a5a64e680c5f320c981b422acb538c4b373d795
Brayan Gacia <bgarcia.95@outlook.com>
2020-09-15 18:10:26 -0600

    Adding dummy data

---
+++ data/dummy-data.js
@@ -0,0 +1,39 @@
+import User from "../model/User";
+
+export const usersData = [
+  new User(
+    "u1",
+    "bgarcia.95",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600204434/Ig-enhanced-pp-pics/pp7_vqatjj.jpg"
+  ),
+  new User(
+    "u2",
+    "angie_nara",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203822/Ig-enhanced-pp-pics/pp1_ftltcb.jpg"
+  ),
+  new User(
+    "u3",
+    "summer_95",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203822/Ig-enhanced-pp-pics/pp6_opiydn.jpg"
+  ),
+  new User(
+    "u4",
+    "julian_scott",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203822/Ig-enhanced-pp-pics/pp4_c8dtm2.jpg"
+  ),
+  new User(
+    "u5",
+    "naomiwatson",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203822/Ig-enhanced-pp-pics/pp5_um80uw.jpg"
+  ),
+  new User(
+    "u6",
+    "gonpachiro",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203822/Ig-enhanced-pp-pics/pp3_zec1et.jpg"
+  ),
+  new User(
+    "u7",
+    "emily_campbell",
+    "https://res.cloudinary.com/bgarcia95/image/upload/v1600203823/Ig-enhanced-pp-pics/pp2_bjygtv.jpg"
+  ),
+];
